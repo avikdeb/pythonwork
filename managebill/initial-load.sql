7c294c0f9a79570fa466969f5c506f74b20ba71e
trainingport <trainingport74@gmail.com>
2018-04-24 21:25:11 +0530

    initial commit -- initial data load

---
+++ managebill/initial-load.sql
@@ -0,0 +1,12 @@
+-- Command to load initial data: sqlite3 managebill.db < initial-load.sql
+
+-- load admin users (super admins)
+INSERT INTO user (userid, username, password, firstname, lastname, email, mobile, adminflag) VALUES (1, 'admin', 'password', 'Admin', 'User', 'avikdeb.select@gmail.com', '9543187077', 1);
+INSERT INTO user (userid, username, password, firstname, lastname, email, mobile, adminflag) VALUES (2, 'admin', 'admin', 'Admin', 'Admin', 'avikdeb.select@gmail.com', '8322965855', 1);
+
+-- load paymentstatus table
+INSERT INTO paymentstatus (paymentstatusid, paymentstatus) VALUES (1, 'Payment Due');
+INSERT INTO paymentstatus (paymentstatusid, paymentstatus) VALUES (2, 'Under Collection');
+INSERT INTO paymentstatus (paymentstatusid, paymentstatus) VALUES (3, 'Payment Done');
+INSERT INTO paymentstatus (paymentstatusid, paymentstatus) VALUES (4, 'On Hold');
+INSERT INTO paymentstatus (paymentstatusid, paymentstatus) VALUES (5, 'Payment Overdue');
