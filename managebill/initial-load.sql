72f4d98f2c5aa5e86a2ae17bcb71b70f7640f591
trainingport <trainingport74@gmail.com>
2018-04-26 11:39:41 +0530

    changed -- col name in SQL

--- managebill/initial-load.sql
+++ managebill/initial-load.sql
@@ -1,8 +1,8 @@
 -- Command to load initial data: sqlite3 managebill.db < initial-load.sql
 
 -- load admin users (super admins)
-INSERT INTO user (userid, username, password, firstname, lastname, email, mobile, adminflag) VALUES (1, 'admin', 'password', 'Admin', 'User', 'avikdeb.select@gmail.com', '9543187077', 1);
-INSERT INTO user (userid, username, password, firstname, lastname, email, mobile, adminflag) VALUES (2, 'admin', 'admin', 'Admin', 'Admin', 'avikdeb.select@gmail.com', '8322965855', 1);
+INSERT INTO user (id, username, password, firstname, lastname, email, mobile, adminflag) VALUES (1, 'admin', 'password', 'Admin', 'User', 'avikdeb.select@gmail.com', '9543187077', 1);
+INSERT INTO user (id, username, password, firstname, lastname, email, mobile, adminflag) VALUES (2, 'admin', 'admin', 'Admin', 'Admin', 'avikdeb.select@gmail.com', '8322965855', 1);
 
 -- load paymentstatus table
 INSERT INTO paymentstatus (paymentstatusid, paymentstatus) VALUES (1, 'Payment Due');
