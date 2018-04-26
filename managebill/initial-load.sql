19a9292298277b1e1b204e41ee5650e0201b1930
trainingport <trainingport74@gmail.com>
2018-04-26 11:41:06 +0530

    changed -- col in SQL

--- managebill/initial-load.sql
+++ managebill/initial-load.sql
@@ -5,8 +5,8 @@ INSERT INTO user (id, username, password, firstname, lastname, email, mobile, ad
 INSERT INTO user (id, username, password, firstname, lastname, email, mobile, adminflag) VALUES (2, 'admin', 'admin', 'Admin', 'Admin', 'avikdeb.select@gmail.com', '8322965855', 1);
 
 -- load paymentstatus table
-INSERT INTO paymentstatus (paymentstatusid, paymentstatus) VALUES (1, 'Payment Due');
-INSERT INTO paymentstatus (paymentstatusid, paymentstatus) VALUES (2, 'Under Collection');
-INSERT INTO paymentstatus (paymentstatusid, paymentstatus) VALUES (3, 'Payment Done');
-INSERT INTO paymentstatus (paymentstatusid, paymentstatus) VALUES (4, 'On Hold');
-INSERT INTO paymentstatus (paymentstatusid, paymentstatus) VALUES (5, 'Payment Overdue');
+INSERT INTO paymentstatus (id, status) VALUES (1, 'Payment Due');
+INSERT INTO paymentstatus (id, status) VALUES (2, 'Under Collection');
+INSERT INTO paymentstatus (id, status) VALUES (3, 'Payment Done');
+INSERT INTO paymentstatus (id, status) VALUES (4, 'On Hold');
+INSERT INTO paymentstatus (id, status) VALUES (5, 'Payment Overdue');
