582d2e58008b3585910c635180529dafe6cd7425
Krishna C G <gkc.cse@gmail.com>
2018-05-14 17:56:04 +0530

    changed by Krishna

--- mypythonscripts/HelloCG.py
+++ mypythonscripts/HelloCG.py
@@ -3,6 +3,6 @@ print("Hello Everyone!")
 def print_something():
 
     for count in range(0,3):
-        print("I am writing something")
+        print("I am writing something!")
 
 print_something()
